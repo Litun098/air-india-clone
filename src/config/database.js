35875354e468e60ef4a035eb9ffa780fb2d5903a
litun098 <nayaklitun9@gmail.com>
2024-05-24 23:41:11 +0530

    bug fixed

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config()
 
-const connect = ()=>{
-    console.log("MongoDB connected requested")
-    return mongoose.connect('mongodb://localhost/project')
-}
+const connect = async () => {
+  console.log("MongoDB connected requested");
+  return await mongoose
+    .connect(
+      process.env.mongoURL
+    )
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
-module.exports ={
-    connect
-}
+module.exports = {
+  connect,
+};
